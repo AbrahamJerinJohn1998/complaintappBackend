72d0fe1241b49ad2153cbe4a6cc12f6ca0deebc7
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-12-09 14:50:19 +0530

    initial

---
+++ src/main/java/com/example/complaintnewappbackend/ComplaintnewappbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.complaintnewappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintnewappbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintnewappbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/complaintnewappbackend/ComplaintnewappbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.complaintnewappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintnewappbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
